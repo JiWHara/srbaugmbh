bb3069d4bffd417fc0b235ff443e1796e0951c6b
JiWHara <jigme.hara@gmail.com>
2023-05-31 03:32:52 -0400

    better menu and styles

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const wideLogoTop = document.querySelector('.topText');
 const wideLogoBottom = document.querySelector('.bottomText');
 const logoTopLine = document.querySelector('.logoTopLine');
 const logoBottomLine = document.querySelector('.logoBottomLine');
+const squareLogo = document.querySelector('.squareLogo');
+const squareText = document.querySelector('.squareText');
+const html = document.querySelector('body');
 
 hamburgerToggle.addEventListener('change', function() {
   if (this.checked) {
@@ -10,10 +13,16 @@ hamburgerToggle.addEventListener('change', function() {
     wideLogoBottom.classList.add('blackText');
     logoTopLine.classList.add('blackLine');
     logoBottomLine.classList.add('blackLine');
+    squareLogo.classList.add('blackBorder');
+    squareText.classList.add('blackText');
+    document.body.style.setProperty('overflow-y', 'hidden');
   } else {
     wideLogoTop.classList.remove('blackText');
     wideLogoBottom.classList.remove('blackText');
     logoTopLine.classList.remove('blackLine');
     logoBottomLine.classList.remove('blackLine');
+    squareLogo.classList.remove('blackBorder');
+    squareText.classList.remove('blackText');
+    document.body.style.setProperty('overflow-y', 'visible');
   }
 });
